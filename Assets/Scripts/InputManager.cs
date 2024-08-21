75ee3f3885ef6a00a4211064978467eae81a60b7
jaeyongpark121 <jaeyongpark01210121@gmail.com>
2024-08-21 22:54:08 +0900

    문자열 가공중

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,27 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    string code;
+    bool onPattern = false;
+    string code = "abcd";
     // Start is called before the first frame update
     void Start()
     {
-        code = PatternManager.instance.MakePattern();
         //Debug.Log(code);
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        //StartCoroutine(GetCode());
+        if (!onPattern)
+        {
+            onPattern = true;
+            StartCoroutine(GetCode());
+        }
     }
 
     IEnumerator GetCode()
     {
-        Debug.Log(PatternManager.instance.MakePattern());
+        code = PatternManager.instance.MakePattern();
+
+        int count = code.Length - 1;
+
+        for (int i = 0; i < code.Length; i++)
+        {
+            //count--;
+            if (count % 2 == 0)
+            {
+                code = code.Insert(count, " ");
+            }
+        }
+
+        Debug.Log(code);
         yield return new WaitForSeconds(1f);
+
+        onPattern = false;
     }
 }
